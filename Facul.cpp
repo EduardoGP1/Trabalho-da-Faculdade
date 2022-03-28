367bec0ca55c5db9e2995ea975f8b63766237b78
Eduardo Pinheiro <96448063+EduardoGP1@users.noreply.github.com>
2022-03-28 01:35:38 -0300

    Atualização malloc

--- Facul.cpp
+++ Facul.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <locale.h>
+#include <stdlib.h>
 
 int menu_principal();
 void menu_compras_de_clientes();
@@ -14,16 +15,19 @@ void exibir_compras_cliente_por_nome();
 void exibir_compras_cliente_por_cpf();
 
 struct cliente{
-	int codigo_cliente=0;
-	char nome[30]="null";
-	int ano_nascimento=0;
-	float total_gastos=0;
-};
+	short int codigo_cliente=0;
+	char nome[30]="NULL";
+	short int ano_nascimento=0; 
+	float montante=0; 
+} cliente[10];
 
-struct cliente cliente[10];
+struct cliente *x= (struct cliente *)malloc(10*sizeof(struct cliente));
 
 int main (){
+	
 setlocale(LC_ALL,"portuguese");
+printf("%d",sizeof(struct cliente));
+
 menu_principal();
 
 system("pause");
@@ -38,13 +42,18 @@ int resposta_valida=0;
 		printf("MENU PRINCIPAL:\n");
 		printf("1 - Incluir cliente\n");
 		printf("2 - Remover cliente\n");
-		printf("3 - Zerar todos os pedidos\n");
-		printf("4 - Listar melhor comprador\n");
-		printf("5 - Exibir compras de um cliente\n");
+		printf("3 - Adicionar compras de um cliente");
+		printf("4 - Zerar todos os pedidos\n");
+		printf("5 - Listar melhor comprador\n");
+		printf("6 - Exibir montante de compras de um cliente\n");
 		printf("Digite o número da opção desejada:");
 		scanf("%d", &menu_principal);
-		if (menu_principal>0&&menu_principal<6)
+		if (menu_principal>0&&menu_principal<6){
 			resposta_valida=1;
+			}
+		else{
+			printf("\nResposta não válida!\n\n");
+		}
 	}
 	while(resposta_valida==0);
 	selecao(menu_principal);
@@ -58,33 +67,60 @@ void selecao(int menu_principal){
 			break;
 		
 		case 2:
-			//remover_cliente();
+			remover_cliente();
 			break;
 		
 		case 3:
+			//adicionar_compras();
+			break;
+		
+		case 4:
 			//zerar_todos_pedidos();
 			break;
 	
-		case 4:
+		case 5:
 			//listar_melhor_comprador();
 			break;
 	
-		case 5:
+		case 6:
 			//exibir_compras_cliente();
 			break;			
 	}
 }
 	
 void incluir_cliente(){
-static int x=0;
+static int numero_do_cliente=0; //numero do cliente=codigo do cliente
+int limite_de_memoria=1;
+int add_espaco=0;
+	if (numero_do_cliente>limite_de_memoria){
+		printf("\nLista de clientes cheia! \nDeseja habilitar mais 10 espaços de memória?\n1 - Sim.\n2 - Não.\nDigite o número de sua resposta:");
+		scanf("%d",&add_espaco);
+		if (add_espaco==1){
+			limite_de_memoria=limite_de_memoria+1;
+			struct cliente *x= (struct cliente *)malloc((limite_de_memoria+1)*sizeof(struct cliente));
+			printf("\nFoi adicionado mais 10 espaços de memória com sucesso!\nJá pode adicionar mais clientes.");
+		}
+		else if (add_espaco==2){
+			printf("\nEspaços de memória continuam cheios. \nRetornando ao Menu Principal...\n\n");
+			menu_principal();
+		}
+		else{
+			printf("\nDesculpe, sua resposta não foi válida!");
+			incluir_cliente();
+		}
+	}
 	printf("\n\nMENU INCLUIR CLIENTE:\n");
 	printf("\nNome do Cliente:");
-	scanf("%s",&cliente[x].nome);
+	scanf("%s",&cliente[numero_do_cliente].nome);
 	printf("Ano de Nascimento:");
-	scanf("%d",&cliente[x].ano_nascimento);
-	printf("\nCliente código %d cadastrado com sucesso!\n\n",x);
-	x++;
+	scanf("%d",&cliente[numero_do_cliente].ano_nascimento);
+	printf("\nCliente código %d cadastrado com sucesso!\n\n",numero_do_cliente);
+	numero_do_cliente++;
 	menu_principal();
+}
+
+void remover_cliente(){
+	printf("nada ainda");
 }
 	
 	
